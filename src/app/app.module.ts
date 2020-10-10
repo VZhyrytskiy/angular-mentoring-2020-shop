6b1642bdedff8867b8b65d2f90397a9ee42c6cb9
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-10 12:27:58 +0300

    added first.component view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [
@@ -12,7 +29,24 @@ import { FirstComponent } from './first/first.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category.enum';
 
 @Component({
   selector: 'app-first',
@@ -8,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponent implements OnInit {
 
   constructor() { }
+
+  name = 'Mi Notebook 15.6';
+  description = '15.6-inch screen, 1920x1080 pixel display. Full-size keyboard. CPU: Intel Core i5-10210U or i7-10510U processor, Quad core up to 4.2GHz-4.9GHz.';
+  price = 1500;
+  category: Category = Category.Notebook;
+  isAvailable = true;
+  rates: number[] = [4, 5, 4.5];
 
   ngOnInit(): void {
   }
