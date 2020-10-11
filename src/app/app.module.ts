4f5e92801d3ccdb2b4d53731c3dbc4f24a80a21d
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-11 19:18:05 +0300

    cart-list.component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { ProductComponent } from './products/components/product/product.component';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
+import { CartListComponent } from './cart/components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductListComponent
+    ProductListComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule,
