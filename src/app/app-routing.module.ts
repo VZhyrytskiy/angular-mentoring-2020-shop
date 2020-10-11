1e39c36e65cc65821394a489bc34ab51a643bf10
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-11 22:28:57 +0300

    simple routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CartListComponent } from './cart/components/cart-list/cart-list.component';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartListComponent },
+  { path: '', component: ProductListComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
