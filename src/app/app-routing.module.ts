e05aa6036fdd47dec847f4bd30c516a0d46eb0e1
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-22 20:14:48 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CartListComponent } from './cart/components/cart-list/cart-list.component';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'cart', component: CartListComponent },
+  { path: 'products', component: ProductListComponent },
   { path: '', component: ProductListComponent, pathMatch: 'full' }
 ];
 
