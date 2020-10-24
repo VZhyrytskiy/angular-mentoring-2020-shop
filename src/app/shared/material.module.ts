31ec5e270f449c2704a7fc65203680db085f9515
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-24 14:38:06 +0300

    add input to material.module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     imports: [
@@ -17,7 +18,8 @@ import { MatListModule } from '@angular/material/list';
         MatButtonModule,
         MatSidenavModule,
         MatCardModule,
-        MatListModule
+        MatListModule,
+        MatInputModule
     ],
     exports: [
         MatToolbarModule,
@@ -27,7 +29,8 @@ import { MatListModule } from '@angular/material/list';
         MatButtonModule,
         MatSidenavModule,
         MatCardModule,
-        MatListModule
+        MatListModule,
+        MatInputModule
     ]
 })
 export class MaterialModule { }
