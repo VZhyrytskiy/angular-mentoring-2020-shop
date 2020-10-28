8a1a739c270504aa61ac0f9cf81b048bce6e8fef
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-10-28 21:31:44 +0300

    shack bar added after item added to cart

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CartService } from 'src/app/cart/services/cart.service';
 
 import { ProductModel } from '../../models/product.model';
@@ -13,10 +14,17 @@ export class ProductListComponent implements OnInit {
 
   products: ProductModel[] = [];
 
-  constructor(private readonly productsService: ProductsService, private readonly cartService: CartService) { }
+  constructor(private readonly productsService: ProductsService,
+    private readonly cartService: CartService,
+    private readonly snackBar: MatSnackBar) { }
 
   onAddedToCart(product: ProductModel): void {
     this.cartService.addProductToCart(product);
+    this.snackBar.open(`Product ${product.name} was added to cart`, null, {
+      duration: 2000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'end'
+    });
   }
 
   ngOnInit(): void {
