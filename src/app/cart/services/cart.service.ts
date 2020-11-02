2c051be152fb1b15226c0a8c48765f05c57f3c5a
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-02 20:40:18 +0300

    method required by task added

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -8,19 +8,28 @@ import { CartItemModel } from '../models/cart-item.model';
 })
 export class CartService {
 
-    private cartItems: CartItemModel[] = [];
+    private _cartItems: CartItemModel[] = [];
+    private _totalQuantity: number = 0;
+    private _totalSum: number = 0;
+
+    get totalQuantity(): number {
+        return this._totalQuantity;
+    }
+
+    get isEmpty(): boolean {
+        return this._totalQuantity === 0;
+    }
+
+    get totalSum(): number {
+        return this._totalSum;
+    }
 
     getCartItems(): ReadonlyArray<CartItemModel> {
-        return this.cartItems;
+        return this._cartItems;
     }
 
     getCartItemTotalPrice(cartItem: CartItemModel): number {
         return cartItem.product.price * cartItem.quantity;
-    }
-
-    getCartTotalPrice(): number {
-        return this.cartItems.map(this.getCartItemTotalPrice)
-            .reduce((prev, next) => prev + next, 0);
     }
 
     addProductToCart(product: ProductModel): void {
@@ -28,15 +37,19 @@ export class CartService {
         if (existingItem !== undefined) {
             this.increaseQuantityByOne(product);
         } else {
-            this.cartItems.push(this.createNewCartItem(product));
+            this._cartItems.push(this.createNewCartItem(product));
+            this.updateCart();
         }
     }
 
     increaseQuantityByOne(product: ProductModel): void {
         const itemToIncrease = this.searchCartForProductByName(product.name);
-        if (itemToIncrease !== undefined) {
-            itemToIncrease.quantity++;
+        if (itemToIncrease === undefined) {
+            return;
         }
+
+        itemToIncrease.quantity++;
+        this.updateCart();
     }
 
     decreaseQuantityByOne(product: ProductModel): void {
@@ -50,15 +63,39 @@ export class CartService {
         } else {
             itemToDecrease.quantity--;
         }
+
+        this.updateCart();
     }
 
     removeProductFromCart(product: ProductModel): void {
         const productToRemove = this.searchCartForProductByName(product.name);
-        this.cartItems = this.cartItems.filter(item => item !== productToRemove);
+        this._cartItems = this._cartItems.filter(item => item !== productToRemove);
+        this.updateCart();
+    }
+
+    removeAllProducts(): void {
+        this._cartItems = [];
+        this.updateCart();
+    }
+
+    updateCart(): void {
+        this.updateTotalSum();
+        this.updateTotalQuantity();
+    }
+
+    private updateTotalSum(): void {
+        this._totalSum = this._cartItems.map(this.getCartItemTotalPrice)
+            .reduce((prev, next) => prev + next, 0);
+    }
+
+    private updateTotalQuantity(): void {
+        this._totalQuantity = this._cartItems
+            .map((item) => { return item.quantity })
+            .reduce((prev, next) => prev + next, 0);
     }
 
     private searchCartForProductByName(productName: string): CartItemModel {
-        return this.cartItems.find(item => item.product.name === productName);
+        return this._cartItems.find(item => item.product.name === productName);
     }
 
     private createNewCartItem(product: ProductModel): CartItemModel {
