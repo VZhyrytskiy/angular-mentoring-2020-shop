0af351e32e25f355b311ffe22c686890aee556d6
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-04 23:51:23 +0300

    added optional param via factory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, Optional, ViewChild } from '@angular/core';
 import { AppConfig } from './shared/services/constants.service';
+import { generatorFactory, GeneratedId } from './shared/services/generator.factory';
+import { GeneratorService } from './shared/services/generator.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: GeneratedId,
+      useFactory: generatorFactory(15),
+      deps: [GeneratorService]
+    }
+  ]
 })
 export class AppComponent implements AfterViewInit {
 
   @ViewChild('appTitle') titleRef: ElementRef<HTMLHeadingElement>;
 
-  constructor(@Inject(AppConfig) private appConfig: AppConfig) { }
+  constructor(@Inject(AppConfig) private readonly appConfig: AppConfig,
+    @Optional() @Inject(GeneratedId) private readonly id: string) {
+    console.log(id);
+  }
 
   ngAfterViewInit(): void {
     this.titleRef.nativeElement.textContent = this.appConfig.title;
