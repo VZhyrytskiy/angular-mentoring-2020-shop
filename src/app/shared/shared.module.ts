af7babb79c237cdebad0c39b7a05844cf8d857db
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-08 16:56:47 +0300

    reorder imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
 
+import { MaterialModule } from './material.module';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { FontAdjustmentDirective } from './directives/font-adjustment/font-adjustment.directive';
 import { OrderByPipe } from './pipes/order-by.pipe';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
