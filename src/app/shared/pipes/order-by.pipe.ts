328ee3b8a6b76df7bb7cb106ad1b9ed542a09e25
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-08 11:53:14 +0300

    impure implementation added

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -1,12 +1,44 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'orderBy'
+  name: 'orderBy',
+  pure: false
 })
 export class OrderByPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  private static getComparator(key: string): (a: object, b: object) => number {
+    if (key.indexOf('.') === -1) {
+      return (a, b) => a[key] > b[key] ? 1 : -1;
+    }
+
+    const keys = key.replace(/\[(\w+)\]/g, '.$1').replace(/^\./, '').split('.');
+
+    return (a, b) => OrderByPipe.getNestedValue(a, keys) > OrderByPipe.getNestedValue(b, keys) ? 1 : -1;
   }
 
+  private static getNestedValue(object: object, keys: string[]): object {
+    keys.forEach(key => {
+      if (!(key in object)) {
+        return false;
+      }
+
+      object = object[key];
+    });
+
+    return object;
+  }
+
+  transform(items: Array<object>, key: string, reverse: boolean = false): Array<object> {
+    if (items?.length === 0) {
+      return items;
+    }
+
+    const sorted = items.sort(OrderByPipe.getComparator(key));
+
+    if (reverse) {
+      return sorted.reverse();
+    }
+
+    return sorted;
+  }
 }
