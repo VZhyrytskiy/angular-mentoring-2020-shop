83e102da6fb9999f503c51a70314424f65ca1368
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-14 23:47:48 +0300

    product-item.component added

---
+++ src/app/products/components/product-item/product-item.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/cart/services/cart.service';
+
+import { ProductModel } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductItemComponent implements OnInit {
+
+  constructor(private router: Router,
+    private readonly cartService: CartService) { }
+
+  @Input() model: ProductModel;
+
+  onViewClick(): void {
+    this.router.navigate([`/product/${this.model.id}`]);
+  }
+
+  onAddToCart(): void {
+    this.cartService.addProductToCart(this.model);
+  }
+
+  ngOnInit(): void { }
+}
