ca650ec623aeae5907180b76295ad6fb08c772ad
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-14 21:08:05 +0300

    index files added

---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,6 @@
+export * from './config-options/config-options.service';
+export * from './constants.service';
+export * from './generator/generator.service';
+export { GeneratedId } from './generator/generator.factory';
+export * from './local-storage/local-storage.service';
+export * from './users.service';
