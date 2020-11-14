32ebbc8bec62917602e307559740e47e95d7ce51
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-14 21:08:30 +0300

    user service and user model added (too late)

---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+
+import { UserModel } from '../models/user.model';
+import { LocalStorageService } from './local-storage/local-storage.service';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private readonly userKey = 'user';
+  private user: BehaviorSubject<UserModel>;
+
+  constructor(private readonly localStorageService: LocalStorageService) {
+    this.user = new BehaviorSubject<UserModel>(this.localStorageService.getItem(this.userKey));
+  }
+
+  login(username: string): Observable<UserModel> {
+    const roles = ['user'];
+
+    if (username === 'admin') {
+      roles.push('admin');
+    }
+
+    const user = new UserModel(username, roles);
+
+    this.localStorageService.setItem<UserModel>(this.userKey, user);
+
+    this.user.next(user);
+
+    return of(user);
+  }
+
+  logout(): Observable<void> {
+    this.localStorageService.removeItem(this.userKey);
+    this.user.next(null);
+
+    return of();
+  }
+
+  isInRole(role: string): Observable<boolean> {
+    return this.getCurrentUser().pipe(map(x => x?.roles.some(userRole => userRole === role)));
+  }
+
+  getCurrentUser(): Observable<UserModel> {
+    return this.user.asObservable();
+  }
+}
