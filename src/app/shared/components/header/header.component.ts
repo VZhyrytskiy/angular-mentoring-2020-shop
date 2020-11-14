be985bf4c026ff870ed7a153c8dc31cc89ca1728
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-15 00:06:09 +0300

    logout added

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
-
 import { MatDialog } from '@angular/material/dialog';
-import { BehaviorSubject, observable, Observable } from 'rxjs';
+
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 import { CartService } from 'src/app/cart/services/cart.service';
 import { LoginComponent } from '../login/login.component';
 import { AppConfig, ConstantsService, UsersService } from './../../services/index';
-
 
 @Component({
   selector: 'app-header',
@@ -28,17 +28,25 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   public totalQuantity: Observable<number>;
 
   constructor(@Inject(AppConfig) private readonly appConfig: AppConfig,
-              public loginDialog: MatDialog,
-              private readonly usersService: UsersService,
-              private readonly cartService: CartService) { }
+    public loginDialog: MatDialog,
+    private readonly usersService: UsersService,
+    private readonly cartService: CartService) { }
 
   onLoginClick(): void {
     this.loginDialog.open(LoginComponent);
   }
 
+  onLogoutClick(): void {
+    this.usersService.logout();
+  }
+
   ngOnInit(): void {
-    this.userName = this.usersService.getCurrentUser().pipe(map(x => x?.username));
-    this.isLoggedIn = this.usersService.getCurrentUser().pipe(map(x => x !== null && x !== undefined));
+    this.userName = this.usersService.getCurrentUser().pipe(map(user => {
+      return user?.username;
+    }));
+    this.isLoggedIn = this.usersService.getCurrentUser().pipe(map(user => {
+      return user !== null && user !== undefined;
+    }));
     this.totalQuantity = this.cartService.totalQuantity();
   }
 
