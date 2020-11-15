964c745b47e14c90f493ccb9d6c4531475b233ec
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-15 17:33:03 +0300

    admin-dashboard.component.html

--- src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { ProductModel } from 'src/app/products/models/product.model';
+import { ProductsService } from 'src/app/products/services/products.service';
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -7,9 +12,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  public products: Observable<ProductModel[]>;
+
+  constructor(private readonly productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.productsService.getProducts();
   }
-
 }
