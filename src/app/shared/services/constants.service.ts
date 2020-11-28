bcc894e1fbed7b995f1839ed5f82079460485978
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-28 20:00:33 +0300

    constants.service update

--- src/app/shared/services/constants.service.ts
+++ src/app/shared/services/constants.service.ts
@@ -2,10 +2,12 @@ import { InjectionToken } from '@angular/core';
 
 export interface AppConfig {
   title: string;
+  darkThemeClassName: string
 }
 
 export const AppConfig = new InjectionToken<AppConfig>('app.config');
 
 export const ConstantsService: AppConfig = {
-  title: 'Shop'
+  title: 'Shop',
+  darkThemeClassName: 'dark'
 };
