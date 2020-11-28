71430cc9bf77dd88dae1c8a932f4f0984ab45aab
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-11-28 20:01:40 +0300

    header update

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { CartService } from 'src/app/cart/services/cart.service';
+import { AppSettingsModel } from '../../models/app-settings.model';
+import { AppSettingsService } from '../../services/app-settings.service';
 import { LoginComponent } from '../login/login.component';
 import { AppConfig, ConstantsService, UsersService } from './../../services/index';
 
@@ -24,16 +27,19 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
   @ViewChild('appTitle') titleRef: ElementRef<HTMLHeadingElement>;
 
-  public userName: Observable<string>;
-  public isLoggedIn: Observable<boolean>;
-  public isAdmin: Observable<boolean>;
-  public totalQuantity: Observable<number>;
+  userName: Observable<string>;
+  isLoggedIn: Observable<boolean>;
+  isAdmin: Observable<boolean>;
+  totalQuantity: Observable<number>;
+  isDarkTheme: Observable<boolean>;
 
-  constructor(@Inject(AppConfig) private readonly appConfig: AppConfig,
-              public loginDialog: MatDialog,
-              private readonly usersService: UsersService,
-              private readonly cartService: CartService,
-              private readonly router: Router) { }
+  constructor(
+    @Inject(AppConfig) private readonly appConfig: AppConfig,
+    private loginDialog: MatDialog,
+    private readonly usersService: UsersService,
+    private readonly cartService: CartService,
+    private readonly router: Router,
+    private readonly settings: AppSettingsService) { }
 
   onLoginClick(): void {
     this.loginDialog.open(LoginComponent);
@@ -42,6 +48,10 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   onLogoutClick(): void {
     this.usersService.logout().subscribe(() =>
       this.router.navigateByUrl(''));
+  }
+
+  onChange(event: MatSlideToggleChange): void {
+    this.settings.setIsDarkTheme(event.checked);
   }
 
   ngOnInit(): void {
@@ -57,6 +67,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
     this.isAdmin = this.usersService.isCurrentUserInRole('admin');
     this.totalQuantity = this.cartService.totalQuantity();
+
   }
 
   ngAfterViewInit(): void {
