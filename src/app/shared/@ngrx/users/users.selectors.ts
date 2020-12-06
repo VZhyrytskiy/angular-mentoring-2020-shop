d0d5a060941cfd31830533a548da10bb91975bbc
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-12-06 18:15:23 +0300

    users.state updated

--- src/app/shared/@ngrx/users/users.selectors.ts
+++ src/app/shared/@ngrx/users/users.selectors.ts
@@ -1,9 +1,13 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { UserModel } from '../../models/user.model';
+import { AppSettingsModel } from '../../models/app-settings.model';
 
+import { UserModel } from '../../models/user.model';
 import { UsersState } from './users.state';
 
 export const selectUsersState = createFeatureSelector<UsersState>('users');
 export const selectUser = createSelector(selectUsersState, (state: UsersState) => state.user);
 export const selectIsAdminUser = createSelector(selectUser,
     (state: UserModel) => state?.roles.some(role => role === 'admin'));
+export const selectUserName = createSelector(selectUser, (state: UserModel) => state?.username);
+export const selectAppSettings = createSelector(selectUsersState, (state: UsersState) => state.appSettings);
+export const selectIsDarkTheme = createSelector(selectAppSettings, (state: AppSettingsModel) => state.isDarkTheme);
