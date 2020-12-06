89a07b2a7e3e17e515b1663a6bb77f8fc12c65f3
Dmitry Struts <dmitry.a.struts@gmail.com>
2020-12-06 16:50:34 +0300

    login reworked

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
-import { ThemeService } from '../../services/theme.service';
-import { UsersService } from './../../services/index';
+import { Actions, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+
+import { take, tap } from 'rxjs/operators';
+
+import { loginUser, loginUserSuccess } from '../../@ngrx';
 
 @Component({
   selector: 'app-login',
@@ -15,31 +19,32 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
   constructor(
-    public dialogRef: MatDialogRef<LoginComponent>,
+    private dialogRef: MatDialogRef<LoginComponent>,
     private formBuilder: FormBuilder,
-    private usersService: UsersService,
-    private themeService: ThemeService
+    private store: Store,
+    private actions$: Actions
   ) { }
 
   onSubmit(): void {
-    if (this.loginForm.invalid) {
-      const controls = this.loginForm.controls;
-      Object.keys(controls)
-        .forEach(name => controls[name].markAsTouched());
+    if (this.loginForm.valid) {
+      this.store.dispatch(loginUser({ username: this.loginForm.value.username }));
       return;
     }
 
-    this.usersService.login(this.loginForm.value.username)
-      .subscribe(user => {
-        this.themeService.restore(user.username);
-        this.dialogRef.close();
-      });
+    const controls = this.loginForm.controls;
+    Object.keys(controls).forEach(name => controls[name].markAsTouched());
   }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required]
     });
+
+    this.actions$.pipe(
+      ofType(loginUserSuccess),
+      tap(() => this.dialogRef.close()),
+      take(1)
+    ).subscribe();
   }
 }
 
